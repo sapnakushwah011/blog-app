838c638c76cac5b754e418728cf23d218b2572a7
Sapna Kushwah <129735749+sapnakushwah011@users.noreply.github.com>
2026-01-04 11:57:46 +0530

    create .env file

--- src/lib/config/db.js
+++ src/lib/config/db.js
@@ -1,6 +1,35 @@
+// import mongoose from "mongoose";
+
+// export const ConnectDB = async () => {
+//     await mongoose.connect('mongodb+srv://sapnakushwahsr_db_user:952273@cluster0.9qgyzyj.mongodb.net/blog-app');
+//     console.log('Database connected');
+// }
+
 import mongoose from "mongoose";
 
-export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://sapnakushwahsr_db_user:SAPNA%409522@cluster0.9qgyzyj.mongodb.net/blog-app');
-    console.log('Database connected');
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const ConnectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+  console.log("Database connected");
+  return cached.conn;
+};
